2e9af55692826e35d7674617d7a20918ea452134
Dev-Bittu <devbittu@proton.me>
2023-08-27 06:12:50 +0530

    Changing secret key

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 from os import environ
 
-SECRET_KEY = environ.get("SECRET_KEY") or "mySecReTK3y"
+SECRET_KEY = environ.get("SECRET_KEY") or "github.com/Dev-Bittu"
 SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI") or "sqlite:///db.sqlite"
